e22bbca82890765994bede1cda939de667baabb9
f-r00t <fruktstav@gmail.com>
2025-05-08 22:24:59 +0200

    Update links

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -3,7 +3,11 @@
         <div class="hero">
             <h1>The new standard for communicating privately.</h1>
             <h2>Encrypted. Decentralized. Open-sourced.</h2>
-            <a href="https://github.com/kryptokrona/hugin-desktop/releases/latest" target="_blank">Download</a>
+            <div class="links">
+              <a class="button" href="https://github.com/kryptokrona/hugin-desktop/releases/latest" target="_blank">Download</a>
+              <a class="store" href="https://play.google.com/store/apps/details?id=com.hugin" target="_blank"><img src="/google.svg" /></a>
+              <a class="store" href="https://apps.apple.com/en/app/hugin-messenger/id6741133299" target="_blank"><img src="/apple.svg" /></a>
+            </div>
         </div>
     </div>
 </div>
@@ -22,21 +26,40 @@
     margin: 0 auto;
     padding: 0 10px;
 
-    a {
+    a.store img {
+      width: 200px;
+      &:hover {
+        filter: brightness(150%);
+        transition: 200ms ease-in-out;
+      }
+    }
+
+    .links {
+      display: flex;
+      flex-direction: row;
+      gap: 1rem; // adds spacing between items
+      flex-wrap: wrap; // optional: allows wrapping on smaller screens
+      justify-content: center; // optional: center-aligns the row
+      align-items: center;
+    }
+
+    a.button {
       display: flex;
       justify-content: center;
-      padding: 10px 15px;
+      padding: 15px 15px;
       min-width: 200px;
-      border-radius: 5px;
+      height: 59.26px;
+      border-radius: 6px;
       font-family: Montserrat, sans-serif;
       font-weight: 600;
       font-size: 1.25rem;
       background-color: rgba(84, 71, 69, 0.10);
-      border: 1px solid #fff;
+      border: 1px solid rgba(255,255,255,0.7);
       color: #fff;
       cursor: pointer;
       transition: 200ms ease-in-out;
       text-decoration: none;
+      margin-top: -3px;
 
       &:hover {
         background-color: rgba(84, 71, 69, 0.30);
