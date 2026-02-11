a6d0bd3381075ecdeb4888a445d31a0d8e700b78
f-r00t <fruktstav@gmail.com>
2026-02-11 11:06:33 +0100

    Update features text

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,54 +1,56 @@
 <script>
-
-import {goto} from "$app/navigation";
+	import { goto } from '$app/navigation';
 </script>
 
 <nav>
-    <div class="container">
-        <div style="display: flex; align-items: center; gap: 1rem; cursor:pointer;" on:click={() => goto("/")}>
-            <img height="36px" width="36px" src="/hugin-icon.png" alt="">
-            <h1>Hugin</h1>
-        </div>
-        <div class="navigation">
-            <a href="/whitepaper" data-sveltekit-prefetch>Whitepaper</a>
-            <a href="https://kryptokrona.org" target="_blank">Kryptokrona -></a>
-        </div>
-    </div>
+	<div class="container">
+		<div
+			style="display: flex; align-items: center; gap: 1rem; cursor:pointer;"
+			on:click={() => goto('/')}
+		>
+			<img height="36px" width="36px" src="/hugin-icon.png" alt="" />
+			<h2>Hugin Messenger</h2>
+		</div>
+		<div class="navigation">
+			<a href="/whitepaper" data-sveltekit-prefetch>Whitepaper</a>
+			<a href="https://kryptokrona.org" target="_blank">Kryptokrona</a>
+		</div>
+	</div>
 </nav>
 
 <style lang="scss">
+	nav {
+		border-bottom: 1px solid var(--border-color);
+		position: fixed;
+		top: 0;
+		background: transparent;
+		backdrop-filter: blur(10px);
+		width: 100%;
+		z-index: 999;
+	}
 
-  nav {
-    border-bottom: 1px solid var(--border-color);
-    position: fixed;
-    top: 0;
-    background: transparent;
-    backdrop-filter: blur(10px);
-    width: 100%;
-    z-index: 999;
-  }
+	.container {
+		height: 60px;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
 
-  .container {
-    height: 60px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
+	.navigation {
+		display: flex;
+		gap: 1rem;
+		align-items: center;
+		font-family: 'Montserrat', sans-serif;
 
-  .navigation {
-    display: flex;
-    gap: 1rem;
-    align-items: center;
+		a {
+			color: var(--text-color);
+			text-decoration: none;
+			opacity: 70%;
+			transition: 100ms ease-in-out;
 
-    a {
-      color: var(--text-color);
-      text-decoration: none;
-      opacity: 70%;
-      transition: 100ms ease-in-out;
-
-    &:hover {
-      opacity: 100%;
-    };
-    }
-  }
+			&:hover {
+				opacity: 100%;
+			}
+		}
+	}
 </style>
